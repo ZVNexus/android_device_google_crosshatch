dejello/android_device_google_bonito

Add final encrpytion blobbies.

device-common.mk
@@ -148,4 +148,6 @@ PRODUCT_COPY_FILES += \
     device/google/bonito/recovery/root/vendor/lib64/hw/android.hardware.boot@1.0-impl.so:recovery/root/vendor/lib64/hw/android.hardware.boot@1.0-impl.so \
     device/google/bonito/recovery/root/sbin/libprotobuf-cpp-full.so:recovery/root/sbin/libprotobuf-cpp-full.so \
     device/google/bonito/recovery/root/sbin/libicuuc.so:recovery/root/sbin/libicuuc.so \
+    device/google/bonito/recovery/root/sbin/libxml2.so:recovery/root/sbin/libxml2.so \
+    device/google/bonito/recovery/root/sbin/libion.so:recovery/root/sbin/libion.so \
     device/google/bonito/recovery/root/sbin/android.hardware.confirmationui@1.0.so:recovery/root/sbin/android.hardware.confirmationui@1.0.so

ZVNexus/android_device_google_crosshatch

device-common.mk
@@ -160,5 +160,7 @@ PRODUCT_COPY_FILES += \
     device/google/crosshatch/recovery/root/vendor/lib64/hw/android.hardware.boot@1.0-impl.so:recovery/root/vendor/lib64/hw/android.hardware.boot@1.0-impl.so \
     device/google/crosshatch/recovery/root/sbin/libprotobuf-cpp-full.so:recovery/root/sbin/libprotobuf-cpp-full.so \
     device/google/crosshatch/recovery/root/sbin/libicuuc.so:recovery/root/sbin/libicuuc.so \
+    device/google/crosshatch/recovery/root/sbin/libxml2.so:recovery/root/sbin/libxml2.so \
+    device/google/crosshatch/recovery/root/sbin/libion.so:recovery/root/sbin/libion.so \
     device/google/crosshatch/recovery/root/sbin/android.hardware.confirmationui@1.0.so:recovery/root/sbin/android.hardware.confirmationui@1.0.so \
     device/google/crosshatch/recovery/root/sbin/ld.config.txt:recovery/root/sbin/ld.config.txt