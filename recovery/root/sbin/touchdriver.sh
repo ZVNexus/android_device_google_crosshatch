ZVNexus/android_device_google_bonito

Grab touch modules from vendor.
Saves space, which as we have come to know is practically a requirement.

recovery/root/sbin/touchdriver.sh
@@ -1,6 +1,9 @@
 #!/sbin/sh
 
-insmod /sbin/synaptics_dsx_core.ko
-insmod /sbin/synaptics_dsx_fw_update.ko
-insmod /sbin/synaptics_dsx_rmi_dev.ko
-insmod /sbin/synaptics_dsx_test_reporting.ko
+SLOT=`getprop ro.boot.slot_suffix`
+mount /dev/block/by-name/vendor$SLOT /vendor -o ro
+insmod /vendor/lib/modules/synaptics_dsx_core.ko
+insmod /vendor/lib/modules/synaptics_dsx_fw_update.ko
+insmod /vendor/lib/modules/synaptics_dsx_rmi_dev.ko
+insmod /vendor/lib/modules/synaptics_dsx_test_reporting.ko
+umount /vendor

ZVNexus/android_device_google_crosshatch

recovery/root/sbin/touchdriver.sh
@@ -1,4 +1,6 @@
 #!/sbin/sh
-
-insmod /sbin/ftm5.ko
-insmod /sbin/sec_touch.ko
+SLOT=`getprop ro.boot.slot_suffix`
+mount /dev/block/by-name/vendor$SLOT /vendor -o ro
+insmod /vendor/lib/modules/ftm5.ko
+insmod /vendor/lib/modules/sec_touch.ko
+umount /vendor