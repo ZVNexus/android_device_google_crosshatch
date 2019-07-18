ZVNexus/android_device_google_bonito

device-common: Sort by file position and name.
Hopefully this is more appealing to the eye.

device-common.mk
@@ -102,55 +102,56 @@ PRODUCT_PACKAGES += \
     android.hardware.dumpstate@1.0-service.bonito
 
 PRODUCT_COPY_FILES += \
-    device/google/bonito/recovery/root/etc/update-payload-key.pub.pem:recovery/root/etc/update-payload-key.pub.pem \
-    device/google/bonito/recovery/root/etc/twrp.fstab:recovery/root/etc/twrp.fstab \
-    device/google/bonito/recovery/root/sbin/prepdecrypt.sh:recovery/root/sbin/prepdecrypt.sh \
-    device/google/bonito/recovery/root/system/etc/vintf/manifest.xml:recovery/root/manifest.xml \
-    device/google/bonito/recovery/root/plat_hwservice_contexts:recovery/root/plat_hwservice_contexts \
-    device/google/bonito/recovery/root/plat_service_contexts:recovery/root/plat_service_contexts \
-    device/google/bonito/recovery/root/vendor_hwservice_contexts:recovery/root/vendor_hwservice_contexts \
-    device/google/bonito/recovery/root/vendor/compatibility_matrix.xml:recovery/root/vendor/compatibility_matrix.xml \
-    device/google/bonito/recovery/root/vendor/compatibility_matrix.1.xml:recovery/root/vendor/compatibility_matrix.1.xml \
-    device/google/bonito/recovery/root/vendor/compatibility_matrix.2.xml:recovery/root/vendor/compatibility_matrix.2.xml \
-    device/google/bonito/recovery/root/vendor/compatibility_matrix.3.xml:recovery/root/vendor/compatibility_matrix.3.xml \
-    device/google/bonito/recovery/root/vendor/compatibility_matrix.device.xml:recovery/root/vendor/compatibility_matrix.device.xml \
-    device/google/bonito/recovery/root/vendor/compatibility_matrix.legacy.xml:recovery/root/vendor/compatibility_matrix.legacy.xml \
-    device/google/bonito/recovery/root/vendor/etc/vintf/manifest.xml:recovery/root/vendor/etc/vintf/manifest.xml \
-    device/google/bonito/recovery/root/vendor/etc/vintf/compatibility_matrix.xml:recovery/root/vendor/etc/vintf/compatibility_matrix.xml \
-    device/google/bonito/recovery/root/sbin/qseecomd:recovery/root/sbin/qseecomd \
-    device/google/bonito/recovery/root/sbin/libdiag.so:recovery/root/sbin/libdiag.so \
-    device/google/bonito/recovery/root/sbin/libdrmfs.so:recovery/root/sbin/libdrmfs.so \
-    device/google/bonito/recovery/root/sbin/libdrmtime.so:recovery/root/sbin/libdrmtime.so \
-    device/google/bonito/recovery/root/sbin/libQSEEComAPI.so:recovery/root/sbin/libQSEEComAPI.so \
-    device/google/bonito/recovery/root/sbin/librpmb.so:recovery/root/sbin/librpmb.so \
-    device/google/bonito/recovery/root/sbin/libssd.so:recovery/root/sbin/libssd.so \
-    device/google/bonito/recovery/root/sbin/libtime_genoff.so:recovery/root/sbin/libtime_genoff.so \
-    device/google/bonito/recovery/root/sbin/libgptutils.so:recovery/root/sbin/libgptutils.so \
-    device/google/bonito/recovery/root/sbin/android.hardware.keymaster@4.0-service-qti:recovery/root/sbin/android.hardware.keymaster@4.0-service-qti \
-    device/google/bonito/recovery/root/vendor/lib64/libkeymasterdeviceutils.so:recovery/root/vendor/lib64/libkeymasterdeviceutils.so \
-    device/google/bonito/recovery/root/vendor/lib64/libkeymasterutils.so:recovery/root/vendor/lib64/libkeymasterutils.so \
-    device/google/bonito/recovery/root/vendor/lib64/libqtikeymaster4.so:recovery/root/vendor/lib64/libqtikeymaster4.so \
-    device/google/bonito/recovery/root/sbin/android.hardware.weaver@1.0-service.citadel:recovery/root/sbin/android.hardware.weaver@1.0-service.citadel \
-    device/google/bonito/recovery/root/sbin/android.hardware.weaver@1.0.so:recovery/root/sbin/android.hardware.weaver@1.0.so \
-    device/google/bonito/recovery/root/vendor/lib64/libnos.so:recovery/root/vendor/lib64/libnos.so \
-    device/google/bonito/recovery/root/vendor/lib64/libnosprotos.so:recovery/root/vendor/lib64/libnosprotos.so \
-    device/google/bonito/recovery/root/vendor/lib64/android.hardware.weaver@1.0-impl.nos.so:recovery/root/vendor/lib64/android.hardware.weaver@1.0-impl.nos.so \
-    device/google/bonito/recovery/root/vendor/lib64/libnos_citadeld_proxy.so:recovery/root/vendor/lib64/libnos_citadeld_proxy.so \
-    device/google/bonito/recovery/root/vendor/lib64/nos_app_weaver.so:recovery/root/vendor/lib64/nos_app_weaver.so \
-    device/google/bonito/recovery/root/vendor/lib64/libnos_datagram.so:recovery/root/vendor/lib64/libnos_datagram.so \
-    device/google/bonito/recovery/root/sbin/vndservicemanager:recovery/root/sbin/vndservicemanager \
-    device/google/bonito/recovery/root/vndservice_contexts:recovery/root/vndservice_contexts \
-    device/google/bonito/recovery/root/sbin/citadeld:recovery/root/sbin/citadeld \
-    device/google/bonito/recovery/root/vendor/lib64/libnos_client_citadel.so:recovery/root/vendor/lib64/libnos_client_citadel.so \
-    device/google/bonito/recovery/root/vendor/lib64/libnos_transport.so:recovery/root/vendor/lib64/libnos_transport.so \
-    device/google/bonito/recovery/root/vendor/lib64/libnos_datagram_citadel.so:recovery/root/vendor/lib64/libnos_datagram_citadel.so \
-    device/google/bonito/recovery/root/sbin/android.hardware.boot@1.0-service:recovery/root/sbin/android.hardware.boot@1.0-service \
-    device/google/bonito/recovery/root/vendor/lib64/hw/bootctrl.sdm710.so:recovery/root/vendor/lib64/hw/bootctrl.sdm710.so \
-    device/google/bonito/recovery/root/vendor/lib64/hw/android.hardware.boot@1.0-impl.so:recovery/root/vendor/lib64/hw/android.hardware.boot@1.0-impl.so \
-    device/google/bonito/recovery/root/sbin/libprotobuf-cpp-full.so:recovery/root/sbin/libprotobuf-cpp-full.so \
-    device/google/bonito/recovery/root/sbin/libicuuc.so:recovery/root/sbin/libicuuc.so \
-    device/google/bonito/recovery/root/sbin/libxml2.so:recovery/root/sbin/libxml2.so \
-    device/google/bonito/recovery/root/sbin/libion.so:recovery/root/sbin/libion.so \
-    device/google/bonito/recovery/root/sbin/update_engine_sideload:recovery/root/sbin/update_engine_sideload \
-    device/google/bonito/recovery/root/sbin/libsecureui.so:recovery/root/sbin/libsecureui.so \
-    device/google/bonito/recovery/root/sbin/android.hardware.confirmationui@1.0.so:recovery/root/sbin/android.hardware.confirmationui@1.0.so
+    $(LOCAL_PATH)/recovery/root/plat_hwservice_contexts:recovery/root/plat_hwservice_contexts \
+    $(LOCAL_PATH)/recovery/root/plat_service_contexts:recovery/root/plat_service_contexts \
+    $(LOCAL_PATH)/recovery/root/vendor_hwservice_contexts:recovery/root/vendor_hwservice_contexts \
+    $(LOCAL_PATH)/recovery/root/vndservice_contexts:recovery/root/vndservice_contexts \
+    $(LOCAL_PATH)/recovery/root/etc/twrp.fstab:recovery/root/etc/twrp.fstab \
+    $(LOCAL_PATH)/recovery/root/etc/update-payload-key.pub.pem:recovery/root/etc/update-payload-key.pub.pem \
+    $(LOCAL_PATH)/recovery/root/sbin/android.hardware.boot@1.0-service:recovery/root/sbin/android.hardware.boot@1.0-service \
+    $(LOCAL_PATH)/recovery/root/sbin/android.hardware.confirmationui@1.0.so:recovery/root/sbin/android.hardware.confirmationui@1.0.so \
+    $(LOCAL_PATH)/recovery/root/sbin/android.hardware.keymaster@4.0-service-qti:recovery/root/sbin/android.hardware.keymaster@4.0-service-qti \
+    $(LOCAL_PATH)/recovery/root/sbin/android.hardware.weaver@1.0.so:recovery/root/sbin/android.hardware.weaver@1.0.so \
+    $(LOCAL_PATH)/recovery/root/sbin/android.hardware.weaver@1.0-service.citadel:recovery/root/sbin/android.hardware.weaver@1.0-service.citadel \
+    $(LOCAL_PATH)/recovery/root/sbin/citadeld:recovery/root/sbin/citadeld \
+    $(LOCAL_PATH)/recovery/root/sbin/ld.config.txt:recovery/root/sbin/ld.config.txt \
+    $(LOCAL_PATH)/recovery/root/sbin/libdiag.so:recovery/root/sbin/libdiag.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libdrmfs.so:recovery/root/sbin/libdrmfs.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libdrmtime.so:recovery/root/sbin/libdrmtime.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libgptutils.so:recovery/root/sbin/libgptutils.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libicuuc.so:recovery/root/sbin/libicuuc.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libion.so:recovery/root/sbin/libion.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libprotobuf-cpp-full.so:recovery/root/sbin/libprotobuf-cpp-full.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libQSEEComAPI.so:recovery/root/sbin/libQSEEComAPI.so \
+    $(LOCAL_PATH)/recovery/root/sbin/librpmb.so:recovery/root/sbin/librpmb.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libsecureui.so:recovery/root/sbin/libsecureui.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libssd.so:recovery/root/sbin/libssd.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libtime_genoff.so:recovery/root/sbin/libtime_genoff.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libxml2.so:recovery/root/sbin/libxml2.so \
+    $(LOCAL_PATH)/recovery/root/sbin/prepdecrypt.sh:recovery/root/sbin/prepdecrypt.sh \
+    $(LOCAL_PATH)/recovery/root/sbin/qseecomd:recovery/root/sbin/qseecomd \
+    $(LOCAL_PATH)/recovery/root/sbin/update_engine_sideload:recovery/root/sbin/update_engine_sideload \
+    $(LOCAL_PATH)/recovery/root/sbin/vndservicemanager:recovery/root/sbin/vndservicemanager \
+    $(LOCAL_PATH)/recovery/root/system/etc/vintf/manifest.xml:recovery/root/manifest.xml \
+    $(LOCAL_PATH)/recovery/root/vendor/compatibility_matrix.1.xml:recovery/root/vendor/compatibility_matrix.1.xml \
+    $(LOCAL_PATH)/recovery/root/vendor/compatibility_matrix.2.xml:recovery/root/vendor/compatibility_matrix.2.xml \
+    $(LOCAL_PATH)/recovery/root/vendor/compatibility_matrix.3.xml:recovery/root/vendor/compatibility_matrix.3.xml \
+    $(LOCAL_PATH)/recovery/root/vendor/compatibility_matrix.device.xml:recovery/root/vendor/compatibility_matrix.device.xml \
+    $(LOCAL_PATH)/recovery/root/vendor/compatibility_matrix.legacy.xml:recovery/root/vendor/compatibility_matrix.legacy.xml \
+    $(LOCAL_PATH)/recovery/root/vendor/compatibility_matrix.xml:recovery/root/vendor/compatibility_matrix.xml \
+    $(LOCAL_PATH)/recovery/root/vendor/etc/vintf/compatibility_matrix.xml:recovery/root/vendor/etc/vintf/compatibility_matrix.xml \
+    $(LOCAL_PATH)/recovery/root/vendor/etc/vintf/manifest.xml:recovery/root/vendor/etc/vintf/manifest.xml \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/android.hardware.weaver@1.0-impl.nos.so:recovery/root/vendor/lib64/android.hardware.weaver@1.0-impl.nos.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libkeymasterdeviceutils.so:recovery/root/vendor/lib64/libkeymasterdeviceutils.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libkeymasterutils.so:recovery/root/vendor/lib64/libkeymasterutils.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libnos.so:recovery/root/vendor/lib64/libnos.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libnos_citadeld_proxy.so:recovery/root/vendor/lib64/libnos_citadeld_proxy.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libnos_client_citadel.so:recovery/root/vendor/lib64/libnos_client_citadel.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libnos_datagram.so:recovery/root/vendor/lib64/libnos_datagram.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libnos_datagram_citadel.so:recovery/root/vendor/lib64/libnos_datagram_citadel.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libnosprotos.so:recovery/root/vendor/lib64/libnosprotos.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libnos_transport.so:recovery/root/vendor/lib64/libnos_transport.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libqtikeymaster4.so:recovery/root/vendor/lib64/libqtikeymaster4.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/nos_app_weaver.so:recovery/root/vendor/lib64/nos_app_weaver.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/hw/android.hardware.boot@1.0-impl.so:recovery/root/vendor/lib64/hw/android.hardware.boot@1.0-impl.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/hw/bootctrl.sdm710.so:recovery/root/vendor/lib64/hw/bootctrl.sdm710.so

ZVNexus/android_device_google_crosshatch

device-common.mk
@@ -114,54 +114,56 @@ PRODUCT_COPY_FILES += \
 
 
 PRODUCT_COPY_FILES += \
-    device/google/crosshatch/recovery/root/etc/twrp.fstab:recovery/root/etc/twrp.fstab \
-    device/google/crosshatch/recovery/root/sbin/prepdecrypt.sh:recovery/root/sbin/prepdecrypt.sh \
-    device/google/crosshatch/recovery/root/system/etc/vintf/manifest.xml:recovery/root/manifest.xml \
-    device/google/crosshatch/recovery/root/plat_hwservice_contexts:recovery/root/plat_hwservice_contexts \
-    device/google/crosshatch/recovery/root/plat_service_contexts:recovery/root/plat_service_contexts \
-    device/google/crosshatch/recovery/root/vendor_hwservice_contexts:recovery/root/vendor_hwservice_contexts \
-    device/google/crosshatch/recovery/root/vendor/compatibility_matrix.xml:recovery/root/vendor/compatibility_matrix.xml \
-    device/google/crosshatch/recovery/root/vendor/compatibility_matrix.1.xml:recovery/root/vendor/compatibility_matrix.1.xml \
-    device/google/crosshatch/recovery/root/vendor/compatibility_matrix.2.xml:recovery/root/vendor/compatibility_matrix.2.xml \
-    device/google/crosshatch/recovery/root/vendor/compatibility_matrix.3.xml:recovery/root/vendor/compatibility_matrix.3.xml \
-    device/google/crosshatch/recovery/root/vendor/compatibility_matrix.device.xml:recovery/root/vendor/compatibility_matrix.device.xml \
-    device/google/crosshatch/recovery/root/vendor/compatibility_matrix.legacy.xml:recovery/root/vendor/compatibility_matrix.legacy.xml \
-    device/google/crosshatch/recovery/root/vendor/etc/vintf/manifest.xml:recovery/root/vendor/etc/vintf/manifest.xml \
-    device/google/crosshatch/recovery/root/vendor/etc/vintf/compatibility_matrix.xml:recovery/root/vendor/etc/vintf/compatibility_matrix.xml \
-    device/google/crosshatch/recovery/root/sbin/qseecomd:recovery/root/sbin/qseecomd \
-    device/google/crosshatch/recovery/root/sbin/libdiag.so:recovery/root/sbin/libdiag.so \
-    device/google/crosshatch/recovery/root/sbin/libdrmfs.so:recovery/root/sbin/libdrmfs.so \
-    device/google/crosshatch/recovery/root/sbin/libdrmtime.so:recovery/root/sbin/libdrmtime.so \
-    device/google/crosshatch/recovery/root/sbin/libQSEEComAPI.so:recovery/root/sbin/libQSEEComAPI.so \
-    device/google/crosshatch/recovery/root/sbin/librpmb.so:recovery/root/sbin/librpmb.so \
-    device/google/crosshatch/recovery/root/sbin/libssd.so:recovery/root/sbin/libssd.so \
-    device/google/crosshatch/recovery/root/sbin/libtime_genoff.so:recovery/root/sbin/libtime_genoff.so \
-    device/google/crosshatch/recovery/root/sbin/libgptutils.so:recovery/root/sbin/libgptutils.so \
-    device/google/crosshatch/recovery/root/sbin/android.hardware.keymaster@4.0-service-qti:recovery/root/sbin/android.hardware.keymaster@4.0-service-qti \
-    device/google/crosshatch/recovery/root/vendor/lib64/libkeymasterdeviceutils.so:recovery/root/vendor/lib64/libkeymasterdeviceutils.so \
-    device/google/crosshatch/recovery/root/vendor/lib64/libkeymasterutils.so:recovery/root/vendor/lib64/libkeymasterutils.so \
-    device/google/crosshatch/recovery/root/vendor/lib64/libqtikeymaster4.so:recovery/root/vendor/lib64/libqtikeymaster4.so \
-    device/google/crosshatch/recovery/root/sbin/android.hardware.weaver@1.0-service.citadel:recovery/root/sbin/android.hardware.weaver@1.0-service.citadel \
-    device/google/crosshatch/recovery/root/sbin/android.hardware.weaver@1.0.so:recovery/root/sbin/android.hardware.weaver@1.0.so \
-    device/google/crosshatch/recovery/root/vendor/lib64/libnos.so:recovery/root/vendor/lib64/libnos.so \
-    device/google/crosshatch/recovery/root/vendor/lib64/libnosprotos.so:recovery/root/vendor/lib64/libnosprotos.so \
-    device/google/crosshatch/recovery/root/vendor/lib64/android.hardware.weaver@1.0-impl.nos.so:recovery/root/vendor/lib64/android.hardware.weaver@1.0-impl.nos.so \
-    device/google/crosshatch/recovery/root/vendor/lib64/libnos_citadeld_proxy.so:recovery/root/vendor/lib64/libnos_citadeld_proxy.so \
-    device/google/crosshatch/recovery/root/vendor/lib64/nos_app_weaver.so:recovery/root/vendor/lib64/nos_app_weaver.so \
-    device/google/crosshatch/recovery/root/vendor/lib64/libnos_datagram.so:recovery/root/vendor/lib64/libnos_datagram.so \
-    device/google/crosshatch/recovery/root/sbin/vndservicemanager:recovery/root/sbin/vndservicemanager \
-    device/google/crosshatch/recovery/root/vndservice_contexts:recovery/root/vndservice_contexts \
-    device/google/crosshatch/recovery/root/sbin/citadeld:recovery/root/sbin/citadeld \
-    device/google/crosshatch/recovery/root/vendor/lib64/libnos_client_citadel.so:recovery/root/vendor/lib64/libnos_client_citadel.so \
-    device/google/crosshatch/recovery/root/vendor/lib64/libnos_transport.so:recovery/root/vendor/lib64/libnos_transport.so \
-    device/google/crosshatch/recovery/root/vendor/lib64/libnos_datagram_citadel.so:recovery/root/vendor/lib64/libnos_datagram_citadel.so \
-    device/google/crosshatch/recovery/root/sbin/android.hardware.boot@1.0-service:recovery/root/sbin/android.hardware.boot@1.0-service \
-    device/google/crosshatch/recovery/root/vendor/lib64/hw/bootctrl.sdm845.so:recovery/root/vendor/lib64/hw/bootctrl.sdm845.so \
-    device/google/crosshatch/recovery/root/vendor/lib64/hw/android.hardware.boot@1.0-impl.so:recovery/root/vendor/lib64/hw/android.hardware.boot@1.0-impl.so \
-    device/google/crosshatch/recovery/root/sbin/libprotobuf-cpp-full.so:recovery/root/sbin/libprotobuf-cpp-full.so \
-    device/google/crosshatch/recovery/root/sbin/libicuuc.so:recovery/root/sbin/libicuuc.so \
-    device/google/crosshatch/recovery/root/sbin/libxml2.so:recovery/root/sbin/libxml2.so \
-    device/google/crosshatch/recovery/root/sbin/libion.so:recovery/root/sbin/libion.so \
-    device/google/crosshatch/recovery/root/sbin/libsecureui.so:recovery/root/sbin/libsecureui.so \
-    device/google/crosshatch/recovery/root/sbin/android.hardware.confirmationui@1.0.so:recovery/root/sbin/android.hardware.confirmationui@1.0.so \
-    device/google/crosshatch/recovery/root/sbin/ld.config.txt:recovery/root/sbin/ld.config.txt
+    $(LOCAL_PATH)/recovery/root/plat_hwservice_contexts:recovery/root/plat_hwservice_contexts \
+    $(LOCAL_PATH)/recovery/root/plat_service_contexts:recovery/root/plat_service_contexts \
+    $(LOCAL_PATH)/recovery/root/vendor_hwservice_contexts:recovery/root/vendor_hwservice_contexts \
+    $(LOCAL_PATH)/recovery/root/vndservice_contexts:recovery/root/vndservice_contexts \
+    $(LOCAL_PATH)/recovery/root/etc/twrp.fstab:recovery/root/etc/twrp.fstab \
+    $(LOCAL_PATH)/recovery/root/etc/update-payload-key.pub.pem:recovery/root/etc/update-payload-key.pub.pem \
+    $(LOCAL_PATH)/recovery/root/sbin/android.hardware.boot@1.0-service:recovery/root/sbin/android.hardware.boot@1.0-service \
+    $(LOCAL_PATH)/recovery/root/sbin/android.hardware.confirmationui@1.0.so:recovery/root/sbin/android.hardware.confirmationui@1.0.so \
+    $(LOCAL_PATH)/recovery/root/sbin/android.hardware.keymaster@4.0-service-qti:recovery/root/sbin/android.hardware.keymaster@4.0-service-qti \
+    $(LOCAL_PATH)/recovery/root/sbin/android.hardware.weaver@1.0.so:recovery/root/sbin/android.hardware.weaver@1.0.so \
+    $(LOCAL_PATH)/recovery/root/sbin/android.hardware.weaver@1.0-service.citadel:recovery/root/sbin/android.hardware.weaver@1.0-service.citadel \
+    $(LOCAL_PATH)/recovery/root/sbin/citadeld:recovery/root/sbin/citadeld \
+    $(LOCAL_PATH)/recovery/root/sbin/ld.config.txt:recovery/root/sbin/ld.config.txt \
+    $(LOCAL_PATH)/recovery/root/sbin/libdiag.so:recovery/root/sbin/libdiag.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libdrmfs.so:recovery/root/sbin/libdrmfs.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libdrmtime.so:recovery/root/sbin/libdrmtime.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libgptutils.so:recovery/root/sbin/libgptutils.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libicuuc.so:recovery/root/sbin/libicuuc.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libion.so:recovery/root/sbin/libion.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libprotobuf-cpp-full.so:recovery/root/sbin/libprotobuf-cpp-full.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libQSEEComAPI.so:recovery/root/sbin/libQSEEComAPI.so \
+    $(LOCAL_PATH)/recovery/root/sbin/librpmb.so:recovery/root/sbin/librpmb.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libsecureui.so:recovery/root/sbin/libsecureui.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libssd.so:recovery/root/sbin/libssd.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libtime_genoff.so:recovery/root/sbin/libtime_genoff.so \
+    $(LOCAL_PATH)/recovery/root/sbin/libxml2.so:recovery/root/sbin/libxml2.so \
+    $(LOCAL_PATH)/recovery/root/sbin/prepdecrypt.sh:recovery/root/sbin/prepdecrypt.sh \
+    $(LOCAL_PATH)/recovery/root/sbin/qseecomd:recovery/root/sbin/qseecomd \
+    $(LOCAL_PATH)/recovery/root/sbin/update_engine_sideload:recovery/root/sbin/update_engine_sideload \
+    $(LOCAL_PATH)/recovery/root/sbin/vndservicemanager:recovery/root/sbin/vndservicemanager \
+    $(LOCAL_PATH)/recovery/root/system/etc/vintf/manifest.xml:recovery/root/manifest.xml \
+    $(LOCAL_PATH)/recovery/root/vendor/compatibility_matrix.1.xml:recovery/root/vendor/compatibility_matrix.1.xml \
+    $(LOCAL_PATH)/recovery/root/vendor/compatibility_matrix.2.xml:recovery/root/vendor/compatibility_matrix.2.xml \
+    $(LOCAL_PATH)/recovery/root/vendor/compatibility_matrix.3.xml:recovery/root/vendor/compatibility_matrix.3.xml \
+    $(LOCAL_PATH)/recovery/root/vendor/compatibility_matrix.device.xml:recovery/root/vendor/compatibility_matrix.device.xml \
+    $(LOCAL_PATH)/recovery/root/vendor/compatibility_matrix.legacy.xml:recovery/root/vendor/compatibility_matrix.legacy.xml \
+    $(LOCAL_PATH)/recovery/root/vendor/compatibility_matrix.xml:recovery/root/vendor/compatibility_matrix.xml \
+    $(LOCAL_PATH)/recovery/root/vendor/etc/vintf/compatibility_matrix.xml:recovery/root/vendor/etc/vintf/compatibility_matrix.xml \
+    $(LOCAL_PATH)/recovery/root/vendor/etc/vintf/manifest.xml:recovery/root/vendor/etc/vintf/manifest.xml \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/android.hardware.weaver@1.0-impl.nos.so:recovery/root/vendor/lib64/android.hardware.weaver@1.0-impl.nos.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libkeymasterdeviceutils.so:recovery/root/vendor/lib64/libkeymasterdeviceutils.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libkeymasterutils.so:recovery/root/vendor/lib64/libkeymasterutils.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libnos.so:recovery/root/vendor/lib64/libnos.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libnos_citadeld_proxy.so:recovery/root/vendor/lib64/libnos_citadeld_proxy.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libnos_client_citadel.so:recovery/root/vendor/lib64/libnos_client_citadel.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libnos_datagram.so:recovery/root/vendor/lib64/libnos_datagram.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libnos_datagram_citadel.so:recovery/root/vendor/lib64/libnos_datagram_citadel.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libnosprotos.so:recovery/root/vendor/lib64/libnosprotos.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libnos_transport.so:recovery/root/vendor/lib64/libnos_transport.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/libqtikeymaster4.so:recovery/root/vendor/lib64/libqtikeymaster4.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/nos_app_weaver.so:recovery/root/vendor/lib64/nos_app_weaver.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/hw/android.hardware.boot@1.0-impl.so:recovery/root/vendor/lib64/hw/android.hardware.boot@1.0-impl.so \
+    $(LOCAL_PATH)/recovery/root/vendor/lib64/hw/bootctrl.sdm845.so:recovery/root/vendor/lib64/hw/bootctrl.sdm845.so