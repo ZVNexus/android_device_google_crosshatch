ZVNexus/android_device_google_bonito

Add libsecureui.so
QSEECOMD as of the July update now requires this blob.

device-common.mk
@@ -152,4 +152,5 @@ PRODUCT_COPY_FILES += \
     device/google/bonito/recovery/root/sbin/libxml2.so:recovery/root/sbin/libxml2.so \
     device/google/bonito/recovery/root/sbin/libion.so:recovery/root/sbin/libion.so \
     device/google/bonito/recovery/root/sbin/update_engine_sideload:recovery/root/sbin/update_engine_sideload \
+    device/google/bonito/recovery/root/sbin/libsecureui.so:recovery/root/sbin/libsecureui.so \
     device/google/bonito/recovery/root/sbin/android.hardware.confirmationui@1.0.so:recovery/root/sbin/android.hardware.confirmationui@1.0.so

ZVNexus/android_device_google_crosshatch

device-common.mk
@@ -162,5 +162,6 @@ PRODUCT_COPY_FILES += \
     device/google/crosshatch/recovery/root/sbin/libicuuc.so:recovery/root/sbin/libicuuc.so \
     device/google/crosshatch/recovery/root/sbin/libxml2.so:recovery/root/sbin/libxml2.so \
     device/google/crosshatch/recovery/root/sbin/libion.so:recovery/root/sbin/libion.so \
+    device/google/crosshatch/recovery/root/sbin/libsecureui.so:recovery/root/sbin/libsecureui.so \
     device/google/crosshatch/recovery/root/sbin/android.hardware.confirmationui@1.0.so:recovery/root/sbin/android.hardware.confirmationui@1.0.so \
     device/google/crosshatch/recovery/root/sbin/ld.config.txt:recovery/root/sbin/ld.config.txt